17f49f1ab7ceba10de89e7a5b2a5d8365a3601a2
Nitish Paradkar <nitishp@umich.edu>
2016-10-09 22:26:28 -0400

    Moves vertically on off centered taps

--- Soccer.js
+++ Soccer.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View,
+        Text,
+        StyleSheet,
+        Dimensions,} from 'react-native';
 
 
 const LC_IDLE = 0;
 const LC_RUNNING = 1;
 const LC_TAPPED = 2;
 const GRAVITY = 0.6;
-const TAPPED_VELOCITY = 10;
+const TAPPED_VELOCITY = 20;
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const BALL_WIDTH = SCREEN_WIDTH * 0.33;
+const BALL_HEIGHT = SCREEN_WIDTH * 0.33;
+const FLOOR_Y = SCREEN_HEIGHT - BALL_HEIGHT;
+const FLOOR_X = SCREEN_WIDTH / 2;
 
 class Soccer extends Component {
 
@@ -14,8 +24,8 @@ class Soccer extends Component {
         super(props);
         this.interval = null;
         this.state = {
-            x: 0,
-            y: 0,
+            x: FLOOR_X,
+            y: FLOOR_Y,
             vx: 0,
             vy: 0,
             lifeCycle: LC_IDLE,
@@ -32,20 +42,46 @@ class Soccer extends Component {
         }
     }
 
-    onTap() {
+    onTap(event) {
+        let centerX = BALL_WIDTH / 2;
+        let centerY = BALL_HEIGHT / 2;
         if(this.state.lifeCycle !== LC_TAPPED) {
+            let velocityX = ((centerX - event.locationX) / SCREEN_WIDTH)
+                                * TAPPED_VELOCITY;
+            console.log(event.locationX - centerX);
+
+            let velocityY = -TAPPED_VELOCITY;
             this.setState({
-                vx: 0, // TODO: Update this to change based on the angle
-                vy: TAPPED_VELOCITY,
+                vx: velocityX,
+                vy: velocityY,
                 lifeCycle: LC_RUNNING,
             })
         }
     }
 
     updatePosition(nextState) {
-        // TODO: Update to not go off the screen (maybe also convert to /s)
         nextState.x += nextState.vx;
         nextState.y += nextState.vy;
+
+        // Hit the left wall
+        if(nextState.x < BALL_WIDTH / 2) {
+            console.log("IN HERE");
+            nextState.vx = -nextState.vx;
+            nextState.x = BALL_WIDTH / 2;
+        }
+
+        // Hit the right wall
+        if(nextState.x > SCREEN_WIDTH - BALL_WIDTH / 2) {
+            nextState.vx = -nextState.vx;
+            nextState.x = SCREEN_WIDTH - BALL_WIDTH / 2;
+        }
+
+        // Reset after falling down
+        if(nextState.y > SCREEN_HEIGHT) {
+            nextState.y = FLOOR_Y;
+            nextState.x = FLOOR_X;
+            nextState.lifeCycle = LC_IDLE;
+        }
     }
 
     updateVelocity(nextState) {
@@ -67,18 +103,24 @@ class Soccer extends Component {
 
     render() {
         var position = {
-            left: this.state.x,
-            top: this.state.y
+            left: this.state.x - (BALL_WIDTH / 2),
+            top: this.state.y - (BALL_HEIGHT / 2),
         }
         return (
-            <Text style={position}>
-                HI
-            </Text>
+            <View style={[styles.ball, position]}
+                  onStartShouldSetResponder={(event) => this.onTap(event.nativeEvent)}>
+                  <Text>HI</Text>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    ball: {
+        width: BALL_WIDTH,
+        height: BALL_HEIGHT,
+        backgroundColor: 'red',
+    },
 });
 
 export default Soccer;
